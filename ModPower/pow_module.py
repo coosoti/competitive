69d3962a3c4f0ca8dec365cb3d5e8a69ec37be7d
coosoti <charlesosoti@gmail.com>
2024-02-02 21:43:07 +0300

    hackerrank pow module python solution

---
+++ ModPower/pow_module.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def mod_power(a, b, m):
+    print(pow(a,b))
+    print(pow(a,b,m))
+    
+if __name__ == '__main__':
+   mod_power(int(input()),int(input()),int(input()))
